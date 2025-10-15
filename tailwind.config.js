be36bd73ac5f38248fbb0f3de515ba27891f1ab7
Kamiye Owaduge <kamiyeowaduge@gmail.com>
2025-10-15 10:04:32 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./moswco2.html"], // include any HTML files you use
+  theme: {
+    extend: {
+      fontFamily: {
+        luckiest: ['Luckiest Guy', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
